2bd3f3c13014e0ce2b720db850f9b58b6f2abf02
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-09-02 16:29:48 -0400

    Create questao-02.cpp

---
+++ atividade-02_struct/questao-02.cpp
@@ -0,0 +1,73 @@
+/*Um determinado Museu de Artes possui inúmeras obras distribuídas em suas galerias.
+A direção resolveu agrupar as informações de cada obra com a seguinte Struct:
+    Código
+    Doação (S/N)
+    Nome da obra
+    Nome do autor
+    Faça um programa que:
+    a) cadastre até 100 obras do museu.
+    b) permita ao usuário fazer consulta às informações cadastradas fornecendo o código da obra.
+Existindo tal obra as informações são exibidas, caso contrário enviar mensagem de
+aviso. A consulta se repete até que o usuário digite código finalizador = 1.*/
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+struct dados{
+    int codigo;
+    char doacao[2], nome[30], autor[50];
+};
+
+int main (){
+    dados artes[100];
+    int i, cont, consul;
+
+    do{
+        cout<<"Informe a quantidade de obras que deseja cadastrar (1-100): ";
+        cin>>cont;
+
+        if(cont < 1 || cont >100){
+            cout<<"\nValor incorreto! Informe um valor no intervalo de 1 a 100\n";
+        }
+    }while(cont < 1 || cont >100);
+
+    cout<<"Informe os dados das Obras."<<endl;
+
+    for(i=0; i<cont; i++){
+        cout<<"Obra "<<i +1<<endl;
+        cout<<"Informe o codigo: ";
+        cin>>artes[i].codigo;
+
+        cout<<"Informe se foi doado (S/N): ";
+        fflush(stdin);
+        gets(artes[i].doacao);
+
+        cout<<"Informe o nome da obra: ";
+        fflush(stdin);
+        gets(artes[i].nome);
+
+        cout<<"Informe o autor: ";
+        fflush(stdin);
+        gets(artes[i].autor);
+
+    }
+
+    do{
+        cout<<"Informe o codigo da obra q deseja consultar ou digite 1 para sair: ";
+        cin>>consul;
+
+        for(i=0; i<cont; i++){
+            if(consul == artes[i].codigo){
+                cout<<"Doado: "<<artes[i].doacao<<endl;
+
+                cout<<"Nome da obra: "<<artes[i].nome<<endl;
+
+                cout<<"Autor: "<<artes[i].autor<<endl;
+            }
+        }
+    }while (consul != 1);
+    
+}
