7a99395dc81d422c9adc0ce2fb3b31ebc92ceb74
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-10-19 17:59:46 -0400

    .

--- passagem_parametro/lista_exercicios/ex-01.cpp
+++ passagem_parametro/lista_exercicios/ex-01.cpp
@@ -4,39 +4,43 @@ notas do aluno; se for P, deverá calcular a média ponderada, com pesos 5, 3 e 
 calculada deverá ser devolvida ao programa principal para, então, ser mostrada.*/
 
 #include <iostream>
+#include <string.h>
 using namespace std;
 
-float media(float n1, float n2, float n3, char a){
+float media(float n1, float n2, float n3, char tp);
+
+int main(){
+    float n1, n2, n3, m;
+    char tp;
+
+    cout<<"Informe a primeira nota: ";
+    cin>>n1;
+
+    cout<<"Informe a segunda nota: ";
+    cin>>n2;
+
+    cout<<"Informe a terceira nota: ";
+    cin>>n3;
+
+    cout<<endl<<"Informe o tipo de media. \nA - media Aritmetica. \nP - media ponderada."<<endl;
+    cin>>tp;
+    m = media(n1, n2, n3, tp);
+
+    cout<<"A media das notas e: "<<m<<endl;
+
+}
+
+float media(float n1, float n2, float n3, char tp){
     float media;
 
-    if(a== 'a' || a=='A'){
+    if(toupper(tp) == 'A'){
         media = (n1 + n2 + n3)/3;
     }else{
-        if(a== 'p' || a=='P'){
+        if(toupper(tp) == 'P'){
             media = (n1*5 + n2*3 + n3*2)/3;
         }else{
             cout<<"As letras informadas devem ser A ou P."<<endl;
         }
     }
     return(media);
-}
-
-int main(){
-    float n1, n2, n3;
-    char a;
-
-    cout<<"Informe a primeira nota: ";
-    cin>>n1;
-
-    cout<<"Informe asegunda nota: ";
-    cin>>n2;
-
-    cout<<"Informe a terceira nota: ";
-    cin>>n3;
-
-    cout<<"Informe uma letra A ou P: ";
-    cin>>a;
-
-    cout<<"A media das notas e: "<<media(n1, n2, n3, a)<<endl;
-
 }
