f67e92a12a4e00f07001aee25e9c16735e7360c0
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-10-19 18:28:43 -0400

    .

--- passagem_parametro/lista_exercicios/ex-02.cpp
+++ passagem_parametro/lista_exercicios/ex-02.cpp
@@ -5,38 +5,52 @@ tempo máximo de duração de um jogo é de duração de um jogo é de 24 horas 
 pode começar em um dia e terminar no outro.*/
 
 #include <iostream>
+#include <string.h>
+
 using namespace std;
 
-int minutos(int h1, int m1, int h2, int m2) {
-    int totalM = 0;
-
-    while (h1 != h2 || m1 != m2) {
-        totalM++;
-        m1++;
-
-        if (m1 == 60) {
-            m1 = 0;
-            h1++;
-        }
-
-        if (h1 == 24) {
-            h1 = 0;
-        }
-    }
-
-    return totalM;
-}
+int tempo_jogo(int h1, int m1, int h2, int m2);
 
 int main() {
-    int h1, m1, h2, m2;
+    int h1, m1, h2, m2, duracao;
 
-    cout<<"Digite a hora de inicio (hh mm): ";
-    cin>>h1>>m1;
+    cout<<"\nInforme a hora de inicio do jogo ";
+    cin>>h1;
 
-    cout<<"Digite a hora de termino (hh mm): ";
-    cin>>h2>>m2;
+    cout<<"\nInforme os minutos de inicio do jogo ";
+    cin>>m1;
 
-    cout << "A duracao do jogo em minutos e: " <<minutos(h1, m1, h2, m2)<< endl;
+    cout<<"\nInforme a hora de termino do jogo ";
+    cin>>h2;
+
+    cout<<"\nInforme os minutos de termino do jogo ";
+    cin>>m2;
+
+    duracao = tempo_jogo(h1, m1, h2, m2);
+
+    cout << "A duracao do jogo em minutos e: " <<duracao<< endl;
 
     return 0;
+}
+
+
+int tempo_jogo(int h1, int m1, int h2, int m2) {
+    float dh, dm, total;
+
+    if(h2 >= h1)
+        dh = h2 - h1;
+    else
+        dh = 24 - h1 + h2;
+
+    if(m2 >= m1)
+        dm = m2 - m1;
+    else
+    {
+        dm = 60 - m1 + m2;
+        dh = dh - 1;
+    }
+
+    total = dh * 60 + dm;
+
+    return total;
 }
