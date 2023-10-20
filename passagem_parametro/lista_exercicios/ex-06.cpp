719f04b6482f8a2d7aced9ffdc031b8ea6144546
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-10-19 21:01:16 -0400

    .

---
+++ passagem_parametro/lista_exercicios/ex-06.cpp
@@ -0,0 +1,10 @@
+/*Construa um programa contendo uma função que receba como parâmetro dois vetores de
+dez elementos inteiros positivos e mostre o vetor união dos dois primeiros.*/
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+
+
---
+++ passagem_parametro/lista_exercicios/ex-07.cpp
@@ -0,0 +1,11 @@
+/*Faça um programa possuindo uma função que receba como parâmetro um vetor A com
+cinco números reais e retorne esses números ordenados de forma crescente.*/
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+
+
+
