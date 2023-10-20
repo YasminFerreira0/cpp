dfad4fda0c44c9bd83b76bfad9088d049d2c7045
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-10-19 21:05:46 -0400

    Create ex-08.cpp

---
+++ passagem_parametro/lista_exercicios/ex-08.cpp
@@ -0,0 +1,5 @@
+/*Faça um programa possuindo uma função que receba dois vetores A e B de dez elementos
+inteiros como parâmetro. A função deverá determinar e mostrar um vetor C que contenha
+os elementos de A e B em ordem decrescente. O vetor C deverá ser mostrado no
+programa principal.*/
+
